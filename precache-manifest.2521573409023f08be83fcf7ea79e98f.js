8d0fe219ae986bdc931ff315859c7b18a4c38f60
antnf3 <antnf3@gmail.com>
2020-09-30 21:40:52 +0900

    Updates

---
+++ precache-manifest.2521573409023f08be83fcf7ea79e98f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917a2e810f16f0caabf9e29cc8ec78e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dac37f1fd2ef82fdbe5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b7e0db0fac78284793fb",
+    "url": "/static/js/2.15e5ad60.chunk.js"
+  },
+  {
+    "revision": "16e28416239a1bb4e6fb38c3a5cb3d7d",
+    "url": "/static/js/2.15e5ad60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dac37f1fd2ef82fdbe5",
+    "url": "/static/js/main.aef0ff09.chunk.js"
+  },
+  {
+    "revision": "1fcdb9be3c569d608915",
+    "url": "/static/js/runtime-main.72836c52.js"
+  }
+]);
