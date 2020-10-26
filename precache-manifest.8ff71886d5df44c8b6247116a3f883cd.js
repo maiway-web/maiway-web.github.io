a0730c7e6727677656364d48b5dc262332c40ba9
celmar <celmargalindez@gmail.com>
2020-10-26 11:02:03 +0800

    Updates

--- precache-manifest.8ff71886d5df44c8b6247116a3f883cd.js
+++ precache-manifest.8ff71886d5df44c8b6247116a3f883cd.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "11dc8c7fcc6d7b7f050c2727ee8e3f67",
+    "revision": "2def3da7a3fec54dbb8426acc43d62bd",
     "url": "/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.924d7c6a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "519993f99808b794e2d3",
-    "url": "/static/js/main.8eb553c7.chunk.js"
+    "revision": "65812a8ed114f3d8a349",
+    "url": "/static/js/main.ee079f3d.chunk.js"
   },
   {
     "revision": "1fcdb9be3c569d608915",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71b8481494243f365f35f4ef3cedb376.js"
+  "/precache-manifest.8ff71886d5df44c8b6247116a3f883cd.js"
 );
 
 self.addEventListener('message', (event) => {
